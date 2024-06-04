9e9fd49f38157a3fb801428706763ce50cc93d8b
Roi20 <roiverano20@gmail.com>
2024-06-04 22:51:10 +0800

    Add project files.

---
+++ AuthenticationLoginAndRegistration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthenticationLoginAndRegistration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
