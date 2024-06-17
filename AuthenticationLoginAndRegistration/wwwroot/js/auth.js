dceb0125e9b401baf69ceee4802cf4b1b12fa708
Roi20 <roiverano20@gmail.com>
2024-06-17 16:30:38 +0800

    Done-Login-And-Registration-Form_Design

--- AuthenticationLoginAndRegistration/wwwroot/js/auth.js
+++ AuthenticationLoginAndRegistration/wwwroot/js/auth.js
@@ -1,1 +1,24 @@
 ﻿
+var input = document.getElementsByTagName("input");
+var submitBtn = document.getElementById("registerSubmit");
+
+submitBtn.onclick = function () {
+
+
+    for (let i = 0; i < input.length; i++) {
+
+        if (input[i].value == "") {
+
+            //input[i].style.borderColor = "red";
+            input[i].style.border = '1px solid red';
+            alert("Complete all input field to proceed");
+            console.log("submit was clicked")
+            break;
+
+        }
+        else {
+            input[i].style.border = 'none';
+        }
+    }
+
+};
