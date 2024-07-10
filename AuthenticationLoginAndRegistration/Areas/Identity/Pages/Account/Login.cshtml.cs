107e44d3719d513acd8722a66f94171c6c2755c8
Roi20 <roiverano20@gmail.com>
2024-07-11 00:29:58 +0800

    Login-And-Registration-Design-Final

--- AuthenticationLoginAndRegistration/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AuthenticationLoginAndRegistration/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -129,7 +129,7 @@ namespace AuthenticationLoginAndRegistration.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Login Attempt failed");
                     return Page();
                 }
             }
